c8c8caf7b3499e13fd2ae4059652f47564a5cd4a
JiwooOh <ohjiwoo123@naver.com>
2021-04-04 02:25:15 +0900

    - add new json data - modify json parsing - modify detail view to display ingredient name

--- Recipe List App/Models/Recipe.swift
+++ Recipe List App/Models/Recipe.swift
@@ -16,7 +16,16 @@ class Recipe : Identifiable, Decodable {
     var prepTime: String
     var cookTime : String
     var servings : Int
-    var ingredients:[String]
+    var highlights:[String]
+    var ingredients:[Ingredient]
     var directions:[String]
     
+}
+
+class Ingredient: Identifiable, Decodable {
+    var id: UUID?
+    var name: String
+    var num: Int?
+    var denom: Int?
+    var unit: String?
 }
