1558021e4c4e3027ceb037f54c1f7700292907fe
JiwooOh <ohjiwoo123@naver.com>
2021-04-04 14:27:30 +0900

    - Change swift list to scrollview, lazy stack and foreach - Hide navigationbartitle and use text element for the title in RecipeListView

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -14,7 +14,15 @@ struct RecipeListView: View {
     var body: some View {
         
         NavigationView {
-            List(model.recipes) { r in
+            
+            VStack (alignment : .leading) {
+            Text("All Recipes")
+                .bold()
+                .padding(.leading)
+                .font(.largeTitle)
+                ScrollView {
+                    LazyVStack(alignment:.leading) {
+                        ForEach(model.recipes) { r in
                 
                 NavigationLink(
                     destination: RecipeDetailView(recipe:r),
@@ -29,13 +37,18 @@ struct RecipeListView: View {
                                 .clipped()
                                 .cornerRadius(5)
                             Text(r.name)
+                                .foregroundColor(.black)
                             
                     }
                     })
                     
                    
+                }
+                    }
+                }
             }
-            .navigationBarTitle("All Recipes")
+            .navigationBarHidden(true)
+            .padding(.leading)
         }
     }
 }
@@ -44,5 +57,6 @@ struct RecipeListView: View {
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
